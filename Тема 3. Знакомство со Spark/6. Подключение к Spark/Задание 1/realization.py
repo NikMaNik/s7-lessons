5367f3d1f873f68102591ea1830da2a4b31afe24
NikMaNik <nikolay.m96@mail.ru>
2025-04-20 02:40:26 +0500

    add.

--- Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/realization.py
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/realization.py
@@ -1,2 +1,9 @@
 from pyspark.sql import SparkSession
-spark = # дополните код
+spark = SparkSession \
+    .builder \
+    .config("spark.executor.memory": "1g") \
+    .config("spark.executor.cores": 2) \
+    .appName("My first session") \
+    .getOrCreate()
+
+         
